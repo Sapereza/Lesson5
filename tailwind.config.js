60ff66f144c9ce09ed6506eead98735e4e322b92
Sapereza <s.perezanderson@gmail.com>
2023-03-29 12:20:28 -0400

    created a 3 page site that is consistant

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}","./index.html","./app.js","./facts.html","./gallery.html","./video.html","./tours.html","./schedule.html","./history.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
